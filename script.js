6dcdc9a3d478f81f2fec1a80e86db8401ae5b4af
Jayden Richardson <jsrich@hotmail.co.nz>
2022-06-28 13:46:58 +1200

    fixed bug with confetti + completed countdown

--- script.js
+++ script.js
@@ -1,26 +1,83 @@
 "use strict";
+/* 
+? Learning JavaScript functions and loops 
+!  Contents:
+*  1. Countdown 
+*  2. Loops
+*  3. Arrays
+*  4. Objects
+*  5. Conditional statements
+*  6. Functions and arrays
+*  7. Functions and objects
+*  8. Functions and conditional statements
+*  9. Functions and loops
+*  10. Functions and arrays
+*/
+
+// * Countdown * //
 
 const countdown = [
-    // countdown array
-    "10",
-    "9",
-    "8",
-    "7",
-    "6",
-    "5",
-    "4",
-    "3",
-    "2",
-    "1",
-    "Liftoff!",
+  // countdown array
+  "10",
+  "9",
+  "8",
+  "7",
+  "6",
+  "5",
+  "4",
+  "3",
+  "2",
+  "1",
+  "Liftoff!",
 ];
 
-for (let i = 0; i < countdown.length; i++) {
-    // loop through the array
-
+function countdownDisplay() {
+  for (let i = 0; i < countdown.length; i++) {
     setTimeout(function () {
-        // setTimeout is a function that takes a function as an argument
-        console.log(countdown[i]);
-        document.getElementById("countdown").innerHTML = countdown[i]; // display the countdown
-    }, 1000 * i); // wait 1 second for each iteration
+      console.log(countdown[i]);
+      document.getElementById("countdown").innerHTML = countdown[i];
+      return countdown[i];
+    }, 1000 * i);
+  }
 }
+
+function celebrate() { 
+    setTimeout(function () { 
+        document.getElementById("yee").innerHTML = "Yee Boi!";  
+        initConfetti();
+        render();
+}, 10000);  
+}
+
+//! Old code attempts to integrate celebration into the countdown
+/*
+var output = document.getElementById("countdown");
+function celebrate() { 
+    if (document.getElementById.innerHTML == "Liftoff!") {    // if the countdown is done, celebrate
+        document.getElementById("yee").innerHTML = "Yee Boi";
+        initConfetti()
+        resizeBy()
+    } else {
+        document.getElementById("yee").innerHTML = "";
+    }
+}
+// call celebrate function
+const yee = document.querySelector("#yee");
+addEventListener.
+if 
+
+function countdownDisplay() {
+  if (countdown[10] === "Liftoff!") {
+    document.querySelector("#yee").innerHTML = "NICE JOB!";
+  } else {
+    for (let i = 0; i < countdown.length; i++) {
+      setTimeout(function () {
+        document.querySelector("#countdown").innerHTML = countdown[i];
+        return countdown[i];
+      }, 1000 * i);
+    }
+  }
+} 
+*/
+
+// *  * //
