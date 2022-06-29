66b62de4e8c19a95d6f6eccb8d4922f61794f179
Jayden Richardson <jsrich@hotmail.co.nz>
2022-06-29 12:44:56 +1200

    update comment above search function

--- script.js
+++ script.js
@@ -154,6 +154,8 @@ function listItems() {
   }
 }
 
+// * Search Array * //
+
 /* Write a simple program that, given a name, searches an array of objects containing names and phone 
 numbers (phonebook) and, if it finds the name, outputs the name and phone 
 number into the paragraph (para) and then exits the loop before it has run its course.  */
